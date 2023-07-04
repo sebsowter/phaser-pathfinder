8f6195fc3d033d2bca9b562b7b6cd25dd602d4a1
Seb Sowter <seb@sebsowter.com>
2023-07-04 21:26:37 +0100

    Refactor

--- src/js/Scene.ts
+++ src/js/Scene.ts
@@ -1,11 +1,11 @@
 import { Pathfinder } from "./Pathfinder";
 
 export class Scene extends Phaser.Scene {
-  public blue: Phaser.GameObjects.Image;
-  public lineGraphics: Phaser.GameObjects.Graphics;
-  public pathFinder: Pathfinder;
-  public pathGroup: Phaser.GameObjects.Group;
-  public red: Phaser.GameObjects.Image;
+  private _blue: Phaser.GameObjects.Image;
+  private _lineGraphics: Phaser.GameObjects.Graphics;
+  private _pathFinder: Pathfinder;
+  private _pathGroup: Phaser.GameObjects.Group;
+  private _red: Phaser.GameObjects.Image;
 
   public preload() {
     this.load.spritesheet("tiles", "./assets/tiles.png", { frameWidth: 8, frameHeight: 8 });
@@ -19,28 +19,27 @@ export class Scene extends Phaser.Scene {
     const layer = tilemap.createLayer(0, tileset, 0, 0);
     const tiles = tilemap.layers[0].data.map((row) => row.map((tile) => tile.index !== 4));
 
-    this.pathGroup = this.add.group({ classType: Phaser.GameObjects.Image, defaultKey: "tiles", defaultFrame: 2 });
-    this.lineGraphics = this.add.graphics().lineStyle(1, 0x999900, 1).setDepth(1);
-    this.red = this.add.image(20, 20, "tiles", 4).setInteractive().setDepth(2);
-    this.blue = this.add.image(108, 108, "tiles", 5).setInteractive().setDepth(2);
+    this._pathGroup = this.add.group({ classType: Phaser.GameObjects.Image, defaultKey: "tiles", defaultFrame: 2 });
+    this._lineGraphics = this.add.graphics().lineStyle(1, 0x999900, 1).setDepth(1);
+    this._red = this.add.image(20, 20, "tiles", 4).setInteractive().setDepth(2);
+    this._blue = this.add.image(108, 108, "tiles", 5).setInteractive().setDepth(2);
 
-    this.pathFinder = new Pathfinder(8, 8).setNodes(tiles);
+    this._pathFinder = new Pathfinder(8, 8).setNodes(tiles);
 
     const snapToGrid = (value: number) => Math.floor(value / 8) * 8 + 4;
 
     let tilecurrent = null;
 
     this.input
-      .setDraggable(this.red)
-      .setDraggable(this.blue)
+      .setDraggable(this._red)
+      .setDraggable(this._blue)
       .on("drag", (_: string, gameObject: Phaser.GameObjects.Image, dragX: number, dragY: number) => {
         const tile = tilemap.getTileAtWorldXY(dragX, dragY, true, camera, layer);
 
         if (tile && tile !== tilecurrent && tile.index !== 4) {
           tilecurrent = tile;
 
-          gameObject.x = snapToGrid(dragX);
-          gameObject.y = snapToGrid(dragY);
+          gameObject.setPosition(snapToGrid(dragX), snapToGrid(dragY));
 
           this.drawPath();
         }
@@ -50,22 +49,22 @@ export class Scene extends Phaser.Scene {
   }
 
   public drawPath(clear = true) {
-    const pointA = new Phaser.Geom.Point(this.red.x, this.red.y);
-    const pointB = new Phaser.Geom.Point(this.blue.x, this.blue.y);
-    const path = this.pathFinder.getPath(pointA, pointB);
+    const pointA = new Phaser.Geom.Point(this._red.x, this._red.y);
+    const pointB = new Phaser.Geom.Point(this._blue.x, this._blue.y);
+    const path = this._pathFinder.getPath(pointA, pointB);
 
     if (clear) {
-      this.lineGraphics.clear();
-      this.pathGroup.clear(true, true);
+      this._lineGraphics.clear();
+      this._pathGroup.clear(true, true);
     }
 
-    this.lineGraphics.moveTo(pointA.x, pointA.y);
+    this._lineGraphics.moveTo(pointA.x, pointA.y);
 
     path.forEach((point) => {
-      this.pathGroup.create(point.x, point.y);
-      this.lineGraphics.lineTo(point.x, point.y);
+      this._pathGroup.create(point.x, point.y);
+      this._lineGraphics.lineTo(point.x, point.y);
     });
 
-    this.lineGraphics.strokePath();
+    this._lineGraphics.strokePath();
   }
 }
